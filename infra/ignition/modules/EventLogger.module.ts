a8b8c1882f394ff532bb1473d8c84a619fb4ea8e
Shivansh Shrivas <shrivasshivansh@gmail.com>
2025-08-16 13:53:27 -0500

    lot of issues

--- infra/ignition/modules/EventLogger.module.ts
+++
@@ -1,7 +0,0 @@
-// ignition/modules/EventLogger.module.ts
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("EventLoggerModule", (m) => {
-  const eventLogger = m.contract("EventLogger", []);
-  return { eventLogger };
-});
